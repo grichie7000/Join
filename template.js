284c4e53a7eedfa313db3cdb24748e3fd5548777
AlkanTetik <alkan.tetik.at@gmail.com>
2025-01-11 14:00:02 +0100

    add files

---
+++ template.js
@@ -0,0 +1,33 @@
+function getSignUpTemplate() {
+    return `
+    <div class="signUp-container">
+    <svg onclick='backToLogin()' class='back-button' width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clip-path="url(#clip0_576_3168)">
+      <path d="M10.4373 14.6667H25.3333C26.0696 14.6667 26.6666 15.2637 26.6666 16.0001C26.6666 16.7364 26.0696 17.3334 25.3333 17.3334H10.4373L16.6466 23.5427C17.1672 24.0634 17.1672 24.9074 16.6466 25.4281C16.126 25.9487 15.2819 25.9487 14.7613 25.4281L6.74746 17.4143C5.96642 16.6332 5.96642 15.3669 6.74747 14.5858L14.7613 6.57206C15.2819 6.05144 16.126 6.05144 16.6466 6.57206C17.1672 7.09268 17.1672 7.93677 16.6466 8.45739L10.4373 14.6667Z" fill="#29ABE2"/>
+      </g>
+      <defs>
+        <clipPath id="clip0_576_3168">
+        <rect width="32" height="32" fill="white"/>
+        </clipPath>
+     </defs>
+    </svg>
+    <div class="headline-container">
+            <h1>Sign Up</h1>
+            <div class="horizontal-line"></div>
+        </div>
+        <div class="input-container">
+            <input type="text" placeholder="Name" class="input-with-icon" required>
+            <input type="password" placeholder="Email" class="input-with-icon" required>
+            <input type="text" placeholder="Password" class="password-input" required>
+            <input type="password" placeholder="Confirm Password" class="password-input" required>
+            <div class='checkbox'>
+              <input type="checkbox" id="" name="" value="">
+              <label>accept the <a>privacy policy</a></label>
+            </div>
+        </div>
+        <div class="signUp-button-container">
+            <div class="signUp-button">Sign Up</div>
+        </div>
+    </div>`;
+}
+
