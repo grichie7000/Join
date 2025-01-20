6a89f5d8d0abfcdaaf07e8df6c943103505eae5a
AlkanTetik <alkan.tetik.at@gmail.com>
2025-01-20 23:01:42 +0100

    updated more files

--- scripts/loadBody.js
+++ scripts/loadBody.js
@@ -5,7 +5,9 @@ function loadBody() {
     setTimeout(() => {
         const headerContent = document.getElementById('header-right-content');
         const loginForm = document.getElementById('loginForm');
+        const footerContent = document.getElementById('footer');
         headerContent.classList.add('show-content');
         loginForm.classList.add('show-content');
+        footerContent.classList.add('show-content');
     }, 300); 
 }
