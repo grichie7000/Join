b9f57441450a3fd9559d0cba686317429e1a8e8b
AlkanTetik <alkan.tetik.at@gmail.com>
2025-01-30 16:58:44 +0100

    added titleButton.png, board.js and defined the titles.

---
+++ scripts/board.js
@@ -0,0 +1,4 @@
+let toDo = document.getElementById('to-do');
+let inProgress = document.getElementById('in-progress');
+let awaitFeedback = document.getElementById('await-feedback');
+let done = document.getElementById('done');
