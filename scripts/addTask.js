528de63a8fc1df6d1ce4b7fa0a617fd0a1032306
grichie7000 <richycatchem@gmail.com>
2025-02-03 12:09:45 +0100

    addTask date

--- scripts/addTask.js
+++ scripts/addTask.js
@@ -1,20 +1,64 @@
-let arrowDirection = true;
-let selectArrow = '';
+let arrowDirectionAssigned = true;
+let arrowDirectionCategory = true;
+let selectArrowAssigned = '';
+let selectArrowCategory = '';
 
 function initAddTask() {
-    console.log('hi');
-    selectArrow = document.getElementById('customArrow');
+    selectArrowAssigned = document.getElementById('customArrowAssigned');
+    selectArrowCategory = document.getElementById('customArrowCategory');
 }
 
-
-function toggleArrow() {
-    const customArrow = document.getElementById('customArrow');
-
-    if (arrowDirection) {
-        customArrow.src = "./assets/img/arrow_up.png";
-    } else {
-        customArrow.src = "./assets/img/arrow_down.png";
+function toggleArrow(selctedInput) {
+    if (selctedInput == 'assigned-to') {
+        changeArrowAssigned();
     }
 
-    arrowDirection = !arrowDirection;
+    if (selctedInput == 'category') {
+        changeArrowCategory();
+    }
+
 }
+
+function changeArrowAssigned() {
+    const customArrowAssigned = document.getElementById('customArrowAssigned');
+
+    if (arrowDirectionAssigned) {
+        customArrowAssigned.src = "./assets/img/arrow_up.png";
+    } else {
+        customArrowAssigned.src = "./assets/img/arrow_down.png";
+    }
+
+    arrowDirectionAssigned = !arrowDirectionAssigned;
+}
+
+function changeArrowCategory(){
+    const customArrowCategory = document.getElementById('customArrowCategory');
+
+    if (arrowDirectionCategory) {
+        customArrowCategory.src = "./assets/img/arrow_up.png";
+    } else {
+        customArrowCategory.src = "./assets/img/arrow_down.png";
+    }
+    
+    arrowDirectionCategory = !arrowDirectionCategory;
+}
+
+
+
+window.addEventListener("click", function (event) {
+    const assignedToDiv = document.getElementById("assigned-to")
+    if (!assignedToDiv.contains(event.target)) {
+        selectArrowAssigned.src = "./assets/img/arrow_down.png";
+        arrowDirectionAssigned = true;
+    }
+});
+
+window.addEventListener("click", function (event) {
+    const categoryDiv = document.getElementById("category")
+    if (!categoryDiv.contains(event.target)) {
+        selectArrowCategory.src = "./assets/img/arrow_down.png";
+        arrowDirectionCategory = true;
+    }
+});
+
+
