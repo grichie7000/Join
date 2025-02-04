69fe3603abb8e67ecf3e98e4cc35aa9d7170223e
grichie7000 <richycatchem@gmail.com>
2025-02-04 09:14:29 +0100

    addTask error messages added

--- scripts/addTask.js
+++ scripts/addTask.js
@@ -1,65 +1,97 @@
-let arrowDirectionAssigned = true;
-let arrowDirectionCategory = true;
-let selectArrowAssigned = '';
-let selectArrowCategory = '';
+let validateIsOk = false;
+let title;
+let errorTitle;
+let date;
+let errorDate;
+let category;
+let errorCategory;
 
 function initAddTask() {
-    selectArrowAssigned = document.getElementById('customArrowAssigned');
-    selectArrowCategory = document.getElementById('customArrowCategory');
+    getElementsByIds()
 }
 
-function toggleArrow(selctedInput) {
-    if (selctedInput == 'assigned-to') {
-        changeArrowAssigned();
+
+function validateFormular(event) {
+    validateTitle(event)
+    validateDate(event)
+    validateCategory(event)
+}
+
+
+function validateTitle(event) {
+    errorTitle.innerHTML = '';
+
+    if (!title.value) {
+        errorTitle.innerHTML = 'This field is required'
+        title.style.border = "2px solid #FF8190"
+        event.preventDefault();
+        return validateIsOk = false
     }
 
-    if (selctedInput == 'category') {
-        changeArrowCategory();
+    return validateIsOk = true;
+}
+
+function validateDate(event) {
+    errorDate.innerHTML = '';
+
+    if (!date.value) {
+        errorDate.innerHTML = 'This field is required'
+        date.style.border = "2px solid #FF8190"
+        console.log(errorDate);
+        event.preventDefault();
+        return validateIsOk = false
     }
+
+    return validateIsOk = true;
+}
+
+
+function validateCategory(event) {
+    errorCategory.innerHTML = '';
+
+    if (!category.value) {
+        errorCategory.innerHTML = 'This field is required'
+        category.style.border = "2px solid #FF8190"
+        event.preventDefault();
+        return validateIsOk = false
+    }
+
+    return validateIsOk = true;
 
 }
 
-function changeArrowAssigned() {
-    const customArrowAssigned = document.getElementById('customArrowAssigned');
+function clearTask() {
+    errorTitle.innerHTML = '';
+    title.style.border = "2px solid #D1D1D1"
 
-    if (arrowDirectionAssigned) {
-        customArrowAssigned.src = "./assets/img/arrow_up.png";
+    errorDate.innerHTML = '';
+    date.style.border = "2px solid #D1D1D1"
+    date.style.color = "#D1D1D1"
+
+    errorCategory.innerHTML = '';
+    category.style.border = "2px solid #D1D1D1"
+}
+
+
+function getElementsByIds() {
+    title = document.getElementById('title');
+    errorTitle = document.getElementById('error-title')
+
+    date = document.getElementById('due-date');
+    errorDate = document.getElementById('error-date')
+
+    category = document.getElementById('category');
+    errorCategory = document.getElementById('error-category')
+}
+
+
+function changeDateColor() {
+    date = document.getElementById('due-date');
+
+    if (date.value) {
+        date.style.color = 'black';
     } else {
-        customArrowAssigned.src = "./assets/img/arrow_down.png";
+        date.style.color = '';
     }
-
-    arrowDirectionAssigned = !arrowDirectionAssigned;
 }
-
-
-function changeArrowCategory(){
-    const customArrowCategory = document.getElementById('customArrowCategory');
-
-    if (arrowDirectionCategory) {
-        customArrowCategory.src = "./assets/img/arrow_up.png";
-    } else {
-        customArrowCategory.src = "./assets/img/arrow_down.png";
-    }
-    
-    arrowDirectionCategory = !arrowDirectionCategory;
-}
-
-
-
-window.addEventListener("click", function (event) {
-    const assignedToDiv = document.getElementById("assigned-to")
-    if (!assignedToDiv.contains(event.target)) {
-        selectArrowAssigned.src = "./assets/img/arrow_down.png";
-        arrowDirectionAssigned = true;
-    }
-});
-
-window.addEventListener("click", function (event) {
-    const categoryDiv = document.getElementById("category")
-    if (!categoryDiv.contains(event.target)) {
-        selectArrowCategory.src = "./assets/img/arrow_down.png";
-        arrowDirectionCategory = true;
-    }
-});
-
 
