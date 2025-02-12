afc38acc8932cdb2a71cf942974229c6426b777f
grichie7000 <richycatchem@gmail.com>
2025-02-12 23:33:58 +0100

    initials js

--- scripts/pushInitals.js
+++ scripts/pushInitals.js
@@ -1,9 +1,28 @@
 const BASE_URL_INITALS = "https://join-d3707-default-rtdb.europe-west1.firebasedatabase.app/";
 
-loadFirebaseData("contatcsFirebase");
+let firebaseDataInitals = [];
 
-async function loadFirebaseData(path = "") {
-    let response = await fetch(BASE_URL_ADDTASK + path + ".json");
-    let responseToJson = await response.json();
-    firebaseData = responseToJson;
+
+function loadInitials(){
+    postInitals();
+}
+
+async function postInitals(path = "", data = {}) {
+    let response = await fetch(BASE_URL_INITALS + path + ".json", {
+        method: "POST",
+        headers: {
+            "initals": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+
+}
+
+
+function submitLogIn(event) {
+    event.preventDefault(); // Verhindert das Standard-Verhalten (Seiten-Neuladen)
+    const dataToBoard = formDataArray.push(document.getElementById('title').value)
+
+
+    postDatatoBoard("/initals", dataToBoard)
 }
